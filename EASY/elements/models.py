54e34e77fd5ff1edc28ce50ec6681ab7c744919d
nkruiz97 <nkruiz1997@gmail.com>
2025-10-18 19:14:40 -0300

    Opciones de subir imagenes a elements en modo debug terminado

--- EASY/elements/models.py
+++ EASY/elements/models.py
@@ -15,15 +15,15 @@ class Type(models.Model):
 
     def __str__(self):
         return self.title
-    
+
 class Element(models.Model):
     title = models.CharField(max_length=255)
-    slug = models.CharField(max_length=255)
-
+    slug = models.CharField(max_length=255, null=True, blank=True)
+    
     description = models.TextField()
-    content = models.TextField()
+    content = models.TextField(null=True, blank=True)
 
-    price = models.DecimalField( max_digits=10, decimal_places=2, default=0.0)
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0.0)
 
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     type = models.ForeignKey(Type, on_delete=models.CASCADE)
@@ -31,7 +31,7 @@ class Element(models.Model):
     created = models.DateField(auto_now_add=True)
     updated = models.DateField(auto_now_add=True)
 
+    image = models.ImageField(upload_to='elements/', blank=True, null=True)
+
     def __str__(self):
         return self.title
-    
-
